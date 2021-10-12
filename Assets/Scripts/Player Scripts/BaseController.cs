c1c738f70d6ce9bd9b14a3cd9b250615c9cdba7b
hackedak <s.athulkrishnan100@gmail.com>
2021-10-12 13:19:51 +0530

    First commit, git initialized, .gitignore added, tank controls added along with path

---
+++ Assets/Scripts/Player Scripts/BaseController.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseController : MonoBehaviour
+{
+    public Vector3 speed;   //for storing speed in x, y and z direction
+    public float xSpeed = 8f, zSpeed = 15f;   
+    public float acceleration = 15f, decelaration = 18f;
+    public float lowSound, normalSound, highSound;
+    public AudioClip engineOnSound, engineOffSound;
+    
+    protected float rotationSpeed = 10f; // used in giving a turning effect for the tank when changing direction
+    protected float maxAngle = 10f;
+
+    private bool isSlow;
+    private AudioSource soundManager;
+
+  // On awake, initialize tank speed
+    void Awake()
+    {
+        soundManager = GetComponent<AudioSource>();
+        speed  = new Vector3(0f,0f, zSpeed);
+    }
+
+    
+
+    // functions for controlling tank
+
+    protected void MoveLeft()
+    {
+        speed = new Vector3(-xSpeed/2f, 0f, speed.z);
+    }
+
+    protected void MoveRight()
+    {
+        speed = new Vector3(xSpeed/2f, 0f, speed.z);
+    }
+
+    protected void MoveStraight()
+    {
+        speed = new Vector3(0f, 0f, speed.z);
+    }
+
+    protected void MoveNormal()
+    {
+        if(isSlow){
+            isSlow = false;
+            soundManager.Stop();
+            soundManager.clip = engineOnSound;
+            soundManager.volume = 0.3f;
+            soundManager.Play();
+        }
+        speed = new Vector3(speed.x, 0f, zSpeed);
+
+    }
+
+    protected void MoveSlow()
+    {
+        if(isSlow){
+            isSlow = true;
+            soundManager.Stop();
+            soundManager.clip = engineOffSound;
+            soundManager.volume = 0.5f;
+            soundManager.Play();
+        }
+        speed = new Vector3(speed.x, 0f, decelaration);
+        
+    }
+
+    protected void MoveFast(){
+        speed = new Vector3(speed.x, 0f, acceleration);
+    }
+
+}// class
---
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : BaseController
+{
+    private Rigidbody myBody;
+
+    void Start()
+    {
+        myBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementKeyboardControl();
+         ChangeRotation();
+    }
+
+    void FixedUpdate() {
+        MoveTank();
+    }
+
+    void MoveTank(){
+        myBody.MovePosition(myBody.position + speed * Time.deltaTime); 
+    }
+
+    void MovementKeyboardControl(){
+        if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A))
+        {
+            MoveLeft();
+        }
+
+        if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D))
+        {
+            MoveRight();
+        }
+
+        if (Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.W))
+        {
+            MoveFast();
+        }
+        if (Input.GetKey (KeyCode.DownArrow) || Input.GetKey (KeyCode.S))
+        {
+            MoveSlow();
+        }
+
+        if (Input.GetKeyUp (KeyCode.LeftArrow) || Input.GetKeyUp (KeyCode.A))
+        {
+            MoveStraight();
+        }
+
+        if (Input.GetKeyUp (KeyCode.RightArrow) || Input.GetKeyUp (KeyCode.D))
+        {
+            MoveStraight();
+        }
+
+        if (Input.GetKeyUp (KeyCode.UpArrow) || Input.GetKeyUp (KeyCode.W))
+        {
+            MoveNormal();
+        }
+        if (Input.GetKeyUp (KeyCode.DownArrow) || Input.GetKeyUp (KeyCode.S))
+        {
+            MoveNormal();
+        }
+
+        
+    }
+
+    void ChangeRotation(){
+        if (speed.x > 0)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, maxAngle, 0f), Time.deltaTime * rotationSpeed);
+        }else if (speed.x < 0)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, -maxAngle, 0f), Time.deltaTime * rotationSpeed);
+        } else{
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, 0f, 0f), Time.deltaTime * rotationSpeed);
+        }
+    }
+
+
+}// class
